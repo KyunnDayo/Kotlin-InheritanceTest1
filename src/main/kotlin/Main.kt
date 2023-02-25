e5c60ba52a1168072253deadb15a4eb15775dd72
KyunnDayo <therealjustineromero@yahoo.com>
2023-02-25 19:58:53 +0800

    git remote add origin https://github.com/KyunnDayo/Kotlin-InheritanceTest1.git git branch -M main git push -u origin main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+import java.util.Scanner
+
+open class person(val name: String, val age: Int){
+    open fun greet(){
+        println("Hello! my name is $name and I am $age years old")
+    }
+}
+
+class classMate(name: String, age: Int, val section: String) : person(name, age){
+    override fun greet(){
+        println("Hello! I am $name from the class of $section, and I am $age years old!")
+    }
+}
+class diffUni(name: String, age:Int, val uni: String,val section:String) : person(name, age){
+    override fun greet(){
+        println("Hello! I am $name from $uni, my section is $section and I am $age years old")
+    }
+}
+class faculty(name: String, age: Int, val uni: String, val facultyNum: Int) : person(name, age){
+    override fun greet(){
+        println("Hello! My name is $name, $age years old, I come from $uni university and my faculty number is $facultyNum")
+    }
+
+}
+
+fun main(){
+    val fred = classMate("Fred", 21,"AN21")
+    val angel = classMate("Angel", 20, "MedTech")
+    val angel2 = diffUni("Angel",20, "DLSM-HSI", "MLS-21")
+    val theo = faculty("Theo", 20, "SJA", 3125412)
+
+    fred.greet()
+    angel2.greet()
+    theo.greet()
+
+}
